9ebf4d18fc07a32b111dfef22ced1bc8ab0fc9eb
nicom <nicomore1128@gmail.com>
2025-04-21 02:17:32 -0500

    creacion carrito_compras.kt y usuario.kt

---
+++ app/src/main/java/com/example/unimarketusc/carrito_compras.kt
@@ -0,0 +1,4 @@
+package com.example.unimarketusc
+
+class carrito_compras {
+}
--- app/src/main/java/com/example/unimarketusc/menu.kt
+++ app/src/main/java/com/example/unimarketusc/menu.kt
@@ -1,11 +1,26 @@
 package com.example.unimarketusc
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class menu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.menu) // Esto carga menu.xml
+        setContentView(R.layout.menu)
+
+        val btnCarrito = findViewById<ImageView>(R.id.btnCarrito)
+        val btnUsuario = findViewById<ImageView>(R.id.btnUsuario)
+
+        btnCarrito.setOnClickListener {
+            val intent = Intent(this, carrito_compras::class.java)
+            startActivity(intent)
+        }
+
+        btnUsuario.setOnClickListener {
+            val intent = Intent(this, usuario::class.java)
+            startActivity(intent)
+        }
     }
 }
