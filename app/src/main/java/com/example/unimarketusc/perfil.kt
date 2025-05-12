8204dd2a7b2e85c3fc55d03a257ae94a4369bcce
nicom <nicomore1128@gmail.com>
2025-05-12 15:35:27 -0500

    edicion categorias

--- app/src/main/java/com/example/unimarketusc/perfil.kt
+++ app/src/main/java/com/example/unimarketusc/perfil.kt
@@ -1,4 +1,11 @@
 package com.example.unimarketusc
 
-class perfil {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class perfil : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.menu)
+    }
 }
