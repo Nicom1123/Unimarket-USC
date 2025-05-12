9372ec181268d30fbc9c54bfe8e1882759ec34c9
nicom <nicomore1128@gmail.com>
2025-05-12 12:07:20 -0500

    modificacion olvidaste_contrasena.kt

--- app/src/main/java/com/example/unimarketusc/olvidaste_contrasena.kt
+++ app/src/main/java/com/example/unimarketusc/olvidaste_contrasena.kt
@@ -1,4 +1,11 @@
 package com.example.unimarketusc
 
-class olvidaste_contrasena {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class olvidaste_contrasena : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.olvidaste_contrasena) // Asegúrate de tener este XML creado
+    }
 }
