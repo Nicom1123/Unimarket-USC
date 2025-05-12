3021abceeaf59f084c94a7db8c130a4d6ed67d3b
nicom <nicomore1128@gmail.com>
2025-05-12 15:13:06 -0500

    edicion categorias

--- app/src/main/java/com/example/unimarketusc/categoria_libreria.kt
+++ app/src/main/java/com/example/unimarketusc/categoria_libreria.kt
@@ -1,11 +1,16 @@
 package com.example.unimarketusc
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class categoria_libreria : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.categoria_libreria)
+        val btnvolver = findViewById<ImageButton>(R.id.Btnvolver)
+        btnvolver.setOnClickListener {
+            finish()
+        }
     }
 }
