f914b0b5cfd40ead1f597b310cf1410eec3bc2a3
nicom <nicomore1128@gmail.com>
2025-05-12 14:19:34 -0500

    modificacion carrito_compras.kt

--- app/src/main/java/com/example/unimarketusc/carrito_compras.kt
+++ app/src/main/java/com/example/unimarketusc/carrito_compras.kt
@@ -1,11 +1,19 @@
 package com.example.unimarketusc
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
-class carrito_compras: AppCompatActivity() {
+class carrito_compras : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.menu)
+        setContentView(R.layout.carrito_compras)
+
+        val btnVolver = findViewById<ImageButton>(R.id.Btnvolver)
+
+        btnVolver.setOnClickListener {
+            finish()
+        }
     }
 }
+
